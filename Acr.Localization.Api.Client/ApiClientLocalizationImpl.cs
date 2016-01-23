da4e5c1860b26c9d665bd7409285f7e3082e1f99
Allan Ritchie <allan.ritchie@gmail.com>
2016-01-23 13:56:26 -0500

    client viewmodel and beginning thoughts on client implementation

--- Acr.Localization.Api.Client/ApiClientLocalizationImpl.cs
+++ Acr.Localization.Api.Client/ApiClientLocalizationImpl.cs
@@ -1,30 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using Acr.Localization.Api.Client.ViewModels;
+using Flurl;
+
 
 namespace Acr.Localization.Api.Client {
 
     public class ApiClientLocalizationImpl : AbstractLocalizationImpl {
-        public string Url { get; set; }
+        public string BaseUrl { get; set; }
+        public string AppName { get; set; }
+        LocalizationPackage package;
 
-        public ApiClientLocalizationImpl(string url)
+
+        public ApiClientLocalizationImpl(string baseUrl, string appName)
         {
-            this.Url = url;
+            this.BaseUrl = baseUrl;
+            this.AppName = appName;
         }
 
 
         public override async Task Initialize()
         {
-            throw new NotImplementedException();
+            // TODO: could monitor locale change to swap this
+            // TODO: write to local cache with expiration date
+            this.package = await this.BaseUrl
+                .AppendPathSegment(this.AppName)
+                .ReceiveJson<LocalizationPackage>();
         }
 
 
         protected override string GetValue(string key, CultureInfo cultureInfo)
         {
-            throw new NotImplementedException();
+            // TODO: make sure we have this package for this locale
+            return String.Empty;
         }
     }
 }
---
+++ Acr.Localization.Api.Client/ViewModels/LocalizationPackage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Acr.Localization.Api.Client.ViewModels
+{
+    public class LocalizationPackage
+    {
+        public string AppName { get; set; }
+        public string LocaleCode { get; set; }
+        public Dictionary<string, string> Values { get; set; } 
+    }
+}
