32727e7d2ba3229dea446cae2948815bbde423fa
Allan Ritchie <allan.ritchie@gmail.com>
2016-06-09 22:11:03 -0400

    add missed code

--- ResxClassGenerator/Program.cs
+++ ResxClassGenerator/Program.cs
@@ -4,10 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Resources;
 using System.Text;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Diagnostics;
 
 
 namespace ResxClassGenerator
@@ -16,13 +12,12 @@ namespace ResxClassGenerator
     {
         static void Main(string[] args)
         {
-            //var cmd = new GenArgs
-            //{
-            //    ResxFilePath = "Strings.resx",
-            //    Class = "Strings",
-            //    Namespace = "Test"
-            //};
-            var cmd = new GenArgs();
+            var cmd = new GenArgs
+            {
+                //ResxFilePath = "Strings.resx",
+                //Class = "Strings",
+                //Namespace = "Test"
+            };
             if (!CommandLine.Parser.Default.ParseArguments(args, cmd))
                 return;
 
@@ -36,7 +31,49 @@ namespace ResxClassGenerator
             File.Delete($"I{cmd.Class}.cs");
             File.Delete($"{cmd.Class}.cs");
 
+            var @class = new StringBuilder();
+            var @int = new StringBuilder();
 
+            @int
+                .AppendLine("using System;")
+                .AppendLine()
+                .AppendLine($"namespace {cmd.Namespace}")
+                .AppendLine("{")
+                .AppendLine($"    public interface I{cmd.Class}")
+                .AppendLine("    {");
+
+            @class
+                .AppendLine("using System;")
+                .AppendLine()
+                .AppendLine($"namespace {cmd.Namespace}.Impl")
+                .AppendLine("{")
+                .AppendLine($"    public class {cmd.Class} : I{cmd.Class}")
+                .AppendLine("    {");
+
+            var values = GetValues(cmd.ResxFilePath);
+            foreach (var value in values)
+            {
+                    @int
+                        .AppendLine("/// <summary>")
+                        .AppendLine($"/// English value is '{value.Item2}'")
+                        .AppendLine("/// </summary>")
+                        .Append($"        public string {value.Item1}")
+                        .Append(" { get; }")
+                        .AppendLine()
+                        .AppendLine();
+
+                    @class
+                        .Append($"        public string {value.Item1}")
+                        .Append(" { get; set; }")
+                        .AppendLine();
+            }
+            @int
+                .AppendLine("    }")
+                .AppendLine("}");
+
+            @class
+                .AppendLine("    }")
+                .AppendLine("}");
 
             Console.WriteLine("Writing Interface & Class Files");
             File.WriteAllText($"I{cmd.Class}.cs", @int.ToString());
